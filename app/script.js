1d235c36f0a39f488b54a630e166263d8ad51489
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-05-21 20:04:14 +0200

    add bell

--- app/script.js
+++ app/script.js
@@ -1,10 +1,37 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { render } from "react-dom";
+
+const statuses = {
+  OFF: "off",
+  REST: "rest",
+  WORK: "work",
+};
+const times = {
+  TWENTY_SEC: 2,
+  TWENT_MIN: 2,
+};
+
+const playBell = () => {
+  const bell = new Audio("./sounds/bell.wav");
+  bell.play();
+};
 
 const App = () => {
   const [status, setStatus] = useState("off");
   const [time, setTime] = useState(20 * 60);
   const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    if (status !== statuses.OFF && time <= 0) {
+      setTime(times.TWENTY_SEC);
+      setStatus((prevStatus) => {
+        if (prevStatus === statuses.WORK) return statuses.REST;
+        return statuses.WORK;
+      });
+      //dzwiek
+      playBell();
+    }
+  }, [time]);
 
   const padTo2Digits = (num) => {
     return num.toString().padStart(2, "0");
@@ -21,8 +48,8 @@ const App = () => {
   }, [time]);
 
   const startTimer = () => {
-    setTime(1200);
-    setStatus("work");
+    setTime(times.TWENT_MIN);
+    setStatus(statuses.WORK);
     setTimer(
       setInterval(() => {
         setTime((time) => time - 1);
@@ -30,10 +57,19 @@ const App = () => {
     );
   };
 
+  const stopTimer = () => {
+    clearInterval(timer);
+    setStatus(statuses.OFF);
+  };
+
+  const closeApp = () => {
+    window.close();
+  };
+
   return (
     <div>
       <h1>Protect your eyes</h1>
-      {status === "off" && (
+      {status === statuses.OFF && (
         <div>
           <p>
             According to optometrists in order to save your eyes, you should
@@ -46,16 +82,22 @@ const App = () => {
           </p>
         </div>
       )}
-      {status === "work" && <img src="./images/work.png" />}
-      {status === "rest" && <img src="./images/rest.png" />}
-      {status !== "off" && <div className="timer">{formatTime}</div>}
-      {status === "off" && (
+      {status === statuses.WORK && <img src="./images/work.png" />}
+      {status === statuses.REST && <img src="./images/rest.png" />}
+      {status !== statuses.OFF && <div className="timer">{formatTime}</div>}
+      {status === statuses.OFF && (
         <button className="btn" onClick={startTimer}>
           Start
         </button>
       )}
-      {status !== "off" && <button className="btn">Stop</button>}
-      <button className="btn btn-close">X</button>
+      {status !== statuses.OFF && (
+        <button className="btn" onClick={stopTimer}>
+          Stop
+        </button>
+      )}
+      <button className="btn btn-close" onClick={closeApp}>
+        X
+      </button>
     </div>
   );
 };
